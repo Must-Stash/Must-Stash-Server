056537f8fe54839d58fdc4b19e0d76f44b19a08a
Taylor Kennedy <taykenned@gmail.com>
2016-05-17 12:05:25 -1000

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
