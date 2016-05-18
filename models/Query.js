9f6e60ce039a0a9ec43c4bb1a6d54736bb5014ca
Taylor Kennedy <taykenned@gmail.com>
2016-05-17 18:50:56 -1000

    adds query endpoint at /api/queries
    adds an endpoint to send queries and activities to server and add to mongodb.

--- models/Query.js
+++ models/Query.js
@@ -4,7 +4,7 @@ const querySchema = new mongoose.Schema({
   query: {type: String, required: true},
   timestamp: {type: Date, required: true},
   tabId: {type: Number, required: true},
-  urls: {type: [String], required: true }
+  urls: {type: [mongoose.Schema.Types.Mixed]}
 });
 
 var Query = mongoose.model('Query', querySchema);
--- routes/api.js
+++ routes/api.js
@@ -6,15 +6,61 @@ const url = require('url');
 
 const Query = require('../models/Query');
 
-router.route('/')
-  .post((req, res, next) => {
-    let  data = req.body.data;
-    res.json({success: data});
-  })
-  .get((req, res, next) => {
-    let query_string = url.parse(req.url, true).query;
-    res.json({success: query_string.q});
+//interaction
+router.post('/queries', (req, res, next) => {
+  let  data = req.body.data;
+
+  let queries = data.filter((request) => {
+    return request.interaction === 'query';
+  }).map((query) => {
+    return {
+      query: query.url,
+      timestamp: query.timestamp,
+      tabId: query.tabId,
+      urls: []
+    };
   });
+
+  // console.log(queries);
+  let activities = data.filter((data) => {
+    return data.interaction === 'activity';
+  }).forEach((activity) => {
+    let found = false;
+    for(let i = queries.length - 1; i >= 0; i--) {
+      if(activity.tabId === queries[i].tabId && activity.timestamp > queries[i].timestamp) {
+        found = true;
+        queries[i].urls.push(activity);
+        break;
+      }
+    }
+
+    Query.insertMany(queries)
+    .then(() => {
+      console.log('ADDED');
+    })
+    .catch((err) => {
+     console.log(err);
+    });
+
+  //   if(!found) {
+  //     let last_query = Query.find({
+  //       tabId: activity.tabId,
+  //       interaction: 'query',
+  //       timestamp: {
+  //         $lt: activity.timestamp
+  //       }
+  //     }).sort('-timestamp').limit(1);
+  //   }
+  //   console.log(last_query);
+  });
+
+  res.json({success: queries});
+});
+
+router.get('/search', (req, res, next) => {
+  let query_string = url.parse(req.url, true).query;
+  res.json({success: query_string.q});
+});
 
 router.post('/history', (req, res, next) => {
   res.json({success: true});
