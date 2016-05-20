e800da10658d6849bc9b06dc467989894aef3272
Taylor Kennedy <taykenned@gmail.com>
2016-05-19 16:05:18 -1000

    removes mongoose and changes to mongodb nodem module
    adds a route for adding queries and activities and uses mongodb node module to insert into mongodb;

--- routes/api.js
+++ routes/api.js
@@ -6,69 +6,102 @@ const url = require('url');
 const request = require('request');
 
 const elastic = require('../lib/elasticsearch');
+const mongo = require('../lib/mongodb');
 
-const Query = require('../models/Query');
-const Activity = require('../models/Activity');
+//const Query = require('../models/Query');
+//const Activity = require('../models/Activity');
 
-router.post('/queries', (req, res, next) => {
+// router.post('/queries', (req, res, next) => {
+//   let data = req.body.data;
+
+//   let queries = [];
+//   let activities = [];
+
+//   for(let i = 0; i < data.length; i++) {
+//     if(data[i].interaction === 'query') {
+//       queries.push(data[i]);
+//     } else if(data[i].interaction === 'activity') {
+//       activities.push(data[i]);
+//     }
+//   }
+
+//   Query.insertMany(queries)
+//   .then((queries) => {
+//     Activity.insertMany(activities)
+//     .then((activities) => {
+//       res.json({success: true});
+//     })
+//     .catch((err) => {
+//       res.json({success: false})
+//     });
+//   })
+//   .catch((err) => {
+//     res.json({success: false})
+//   });
+
+// });
+
+router.post('/qa', (req, res, next) => {
   let data = req.body.data;
 
-  let queries = [];
-  let activities = [];
-
   for(let i = 0; i < data.length; i++) {
-    if(data[i].interaction === 'query') {
-      queries.push(data[i]);
-    } else if(data[i].interaction === 'activity') {
-      activities.push(data[i]);
+    let query = data[i].query;
+    let activity = data[i].activity;
+
+    if(query.tabId === activity.tabId && query.timeStamp < activity.timeStamp) {
+      request(data[i].url, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+          elastic.addUrls(response.request.uri.href, body)
+          .then((response) => {
+            res.json({success: response.hits.hits});
+          })
+          .catch((err) => {
+            res.json({success: false});
+          });
+        }
+      });
     }
   }
 
-  Query.insertMany(queries)
-  .then((queries) => {
-    Activity.insertMany(activities)
-    .then((activities) => {
-      res.json({success: true});
-    })
-    .catch((err) => {
-      res.json({success: false})
-    });
-  })
-  .catch((err) => {
-    res.json({success: false})
+  mongo.addQA(data, (err, response) => {
+    if(err) {
+      return res.json({success: false});
+    }
+    res.json({success: true});
   });
 
+  // for(let i = 0; i < data.length; i++) {
+  //   request(data[i].url, (error, response, body) => {
+  //     if (!error && response.statusCode == 200) {
+  //       elastic.addUrls(response.request.uri.href, body)
+  //       .then((response) => {
+  //         res.json({success: response.hits.hits});
+  //       })
+  //       .catch((err) => {
+  //         res.json({success: false})
+  //       });
+  //     }
+  //   });
+  // }
 });
 
-router.post('/activities', (req, res, next) => {
-  let data = req.body.data;
-  for(let i = 0; i < data.length; i++) {
-    request(data[i].url, (error, response, body) => {
-      if (!error && response.statusCode == 200) {
-        elastic.addUrls(response.request.uri.href, body)
-        .then((response) => {
-          res.json({success: response.hits.hits});
-        })
-        .catch((err) => {
-          res.json({success: false})
-        });
-      }
-    });
-  }
-  res.json({success: false});
+router.get('/qa', (req, res, next) => {
+  mongo.getQA((err, response) => {
+    if(err) {
+      return res.json({success: false});
+    }
+    res.json({success: response});
+  });
 });
 
 router.get('/search', (req, res, next) => {
   let query_string = url.parse(req.url, true).query.q;
-  console.log("QUERY STRING", query_string);
   elastic.getUrls(query_string)
   .then((response) => {
-    console.log("SEARCH RESPONSE");
     res.json({success: response.hits.hits});
   })
   .catch((err) => {
-    console.log("SEARCH ERROR", err);
-    res.json({success: false})
+    res.json({success: false});
   });
 });
 
--- server.js
+++ server.js
@@ -2,19 +2,22 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const mongo = require('./lib/mongodb');
 
 const api = require('./routes/api');
 
-mongoose.connect("mongodb://localhost:27017/muststash");
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("mongoose connected!");
+mongo.connect('mongodb://localhost:27017/muststash', () => {
+  console.log('mongo connected');
 });
+//mongoose.connect("mongodb://localhost:27017/muststash");
+//const db = mongoose.connection;
+
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log("mongoose connected!");
+// });
 
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
 app.use(bodyParser.json({limit: '50mb'}));
