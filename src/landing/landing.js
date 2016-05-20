7b0e0ccdac44502ee46ecc3af57de7ec096713a1
ypyang237 <ypyang237@gmail.com>
2016-05-19 17:07:29 -1000

    populated list.js with set state and ajax call

--- src/landing/landing.js
+++ src/landing/landing.js
@@ -1,12 +1,39 @@
 'use strict';
 
 const React       = require('react'),
+      $           = require('jquery'),
       Nav         = require('./nav.js'),
       Header      = require('./header.js'),
       List      = require('./list.js')
       ;
 
 const LandingPage = React.createClass({
+
+  getInitialState: function() {
+      return {
+        urlList: []
+      };
+  },
+
+  loadDataFromServer: function() {
+    $.ajax({
+      url: "http://127.0.0.1:3000/api/search?q=" + encodeURIComponent('cats'),
+      method: 'GET',
+      cache: false,
+      success: function(data) {
+        this.setState({
+          urlList: data
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error("Error")
+      }.bind(this)
+    })
+  },
+  componentDidMount: function() {
+    this.loadDataFromServer();
+  },
+
 
   render: function() {
 
@@ -15,7 +42,7 @@ const LandingPage = React.createClass({
         <h1> Landing Page</h1>
         <Nav />
         <Header />
-        <List />
+        <List list={this.state.urlList} />
       </div>
 
       )
