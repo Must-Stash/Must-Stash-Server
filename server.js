3fe8970ab99a2e950eee94975b836e43b212d8bd
Owen <owenyangg@gmail.com>
2016-05-23 10:26:30 -1000

    Adds semicolon

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongo.connect('mongodb://localhost:27017/muststash', () => {
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
 app.use(bodyParser.json({limit: '50mb'}));
 
-app.use('/api', api)
+app.use('/api', api);
 
 app.use(express.static(__dirname + '/public'));
 
